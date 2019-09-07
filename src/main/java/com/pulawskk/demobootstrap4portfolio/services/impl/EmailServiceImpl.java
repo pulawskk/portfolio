cd18ec4e7dd8e3e6d538c83f3167bdb1da4dd4a5
KarolPulawski <karol.j.pulawski@gmail.com>
2019-09-07 21:19:40 +0200

    Add sending email feature.

---
+++ src/main/java/com/pulawskk/demobootstrap4portfolio/services/impl/EmailServiceImpl.java
@@ -0,0 +1,27 @@
+package com.pulawskk.demobootstrap4portfolio.services.impl;
+
+import com.pulawskk.demobootstrap4portfolio.services.EmailService;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+
+    private JavaMailSender javaMailSender;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public void sendSimpleMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        message.setFrom("pulawskk@gmail.com");
+
+        javaMailSender.send(message);
+    }
+}
