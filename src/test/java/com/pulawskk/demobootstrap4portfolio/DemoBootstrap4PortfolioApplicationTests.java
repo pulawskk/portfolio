b83e43c424e67d1b3584f9b2304db573ec7bc114
KarolPulawski <karol.j.pulawski@gmail.com>
2019-09-13 00:02:57 +0200

    Back test with context loading to JUnit 4. #11

--- src/test/java/com/pulawskk/demobootstrap4portfolio/DemoBootstrap4PortfolioApplicationTests.java
+++ src/test/java/com/pulawskk/demobootstrap4portfolio/DemoBootstrap4PortfolioApplicationTests.java
@@ -1,16 +1,19 @@
 package com.pulawskk.demobootstrap4portfolio;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+//@ExtendWith(SpringExtension.class)
+//@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
 public class DemoBootstrap4PortfolioApplicationTests {
 
     @Test
-    public void contextLoads() {
+        public void contextLoads() {
     }
 
 }
