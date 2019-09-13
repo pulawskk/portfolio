671fabc4432d072cd92def6a32b0ff21e35c1cee
KarolPulawski <karol.j.pulawski@gmail.com>
2019-09-13 18:57:55 +0200

    Back test with context loading to JUnit 5 and refactor spring @Config.

--- src/main/java/com/pulawskk/demobootstrap4portfolio/AppConfigForJasyptStarter.java
+++ src/main/java/com/pulawskk/demobootstrap4portfolio/AppConfigForJasyptStarter.java
@@ -3,7 +3,7 @@ package com.pulawskk.demobootstrap4portfolio;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@PropertySource("encrypted.properties")
+//@Configuration
+//@PropertySource("encrypted.properties")
 public class AppConfigForJasyptStarter {
 }
