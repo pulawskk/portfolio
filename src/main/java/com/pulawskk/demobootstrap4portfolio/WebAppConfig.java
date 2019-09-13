c5c3721ff690f5ae41f0387935f81a5ab7222b68
KarolPulawski <karol.j.pulawski@gmail.com>
2019-09-13 18:57:55 +0200

    Back test with context loading to JUnit 5 and refactor spring @Config. #11

--- src/main/java/com/pulawskk/demobootstrap4portfolio/WebAppConfig.java
+++ src/main/java/com/pulawskk/demobootstrap4portfolio/WebAppConfig.java
@@ -4,11 +4,13 @@ import com.pulawskk.demobootstrap4portfolio.services.PropertyServiceForJasyptSta
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@PropertySource("encrypted.properties")
 public class WebAppConfig extends WebMvcConfigurerAdapter {
 
     @Bean
--- src/test/java/com/pulawskk/demobootstrap4portfolio/DemoBootstrap4PortfolioApplicationTests.java
+++ src/test/java/com/pulawskk/demobootstrap4portfolio/DemoBootstrap4PortfolioApplicationTests.java
@@ -1,19 +1,18 @@
 package com.pulawskk.demobootstrap4portfolio;
 
-import org.junit.Test;
+import com.ulisesbocchio.jasyptspringboot.properties.JasyptEncryptorConfigurationProperties;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
-public class DemoBootstrap4PortfolioApplicationTests {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = { AppConfigForJasyptStarter.class })
+class DemoBootstrap4PortfolioApplicationTests {
 
     @Test
-        public void contextLoads() {
+        void contextLoads() {
     }
 
 }
