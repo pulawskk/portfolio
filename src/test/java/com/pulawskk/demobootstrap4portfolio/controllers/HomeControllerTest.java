edb648665d9cf15f9b8c31b3cd82769075203794
drogba <pulawskk@gmail.com>
2020-06-19 19:03:37 +0200

    #3 write unit test for HomeController

---
+++ src/test/java/com/pulawskk/demobootstrap4portfolio/controllers/HomeControllerTest.java
@@ -0,0 +1,35 @@
+package com.pulawskk.demobootstrap4portfolio.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class HomeControllerTest {
+
+    @InjectMocks
+    HomeController homeController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
+    }
+
+    @Test
+    void shouldDisplayIndexPage_whenIndexPageIsCalled() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
