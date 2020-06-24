fe7bc792014793b2a92fb79ac878d445b7a32917
drogba <pulawskk@gmail.com>
2020-06-24 22:21:28 +0200

    #7 complete endpoint /download

--- src/main/java/com/pulawskk/demobootstrap4portfolio/controllers/HomeController.java
+++ src/main/java/com/pulawskk/demobootstrap4portfolio/controllers/HomeController.java
@@ -1,13 +1,31 @@
 package com.pulawskk.demobootstrap4portfolio.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HomeController {
 
+    @Value("classpath:data/cv.pdf")
+    private Resource resourceFile;
+
     @GetMapping({"/"})
     public String indexPage() {
         return "index";
+    }
+
+    @GetMapping("/download")
+    @ResponseBody
+    public ResponseEntity downloadCv() {
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=cv.pdf")
+                .body(resourceFile);
     }
 }
